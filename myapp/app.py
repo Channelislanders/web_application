95b8499816281e879f2cd8404c7124268ac12685
p-park6 <pattypark6@gmail.com>
2024-05-15 15:42:06 -0700

    fixed the website's layout

--- myapp/app.py
+++ myapp/app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import xarray as xr
+import shinyswatch
 
 from shiny import App, Inputs, Outputs, Session, reactive, render, req, ui
 #from shared import ds_20C
@@ -21,13 +22,13 @@ merge_test = xr.merge([temp, o2, salt])
 merge_array = merge_test#.to_array()
 
 
-climate_variable_choices = [
+climate_variable_choices_TEMP = [
     "SALT",
     "O2",
     "TEMP"
 ]
 
-climate_variable_choices = [
+climate_variable_choices_SST = [
     "SALT",
     "O2",
     "TEMP"
@@ -45,21 +46,53 @@ climate_experiment_choices = [
     "min"
 ]
 
-app_ui = ui.page_navbar(
+app_ui = ui.page_navbar(shinyswatch.theme.sandstone(),
     ui.nav_spacer(),
     ui.nav_panel(
         "About", 
-        ui.output_image("ch_image"),
+        ui.layout_columns(
+        ui.output_image("ch_image", inline=True),
         ui.markdown(
         """
-        The Channel Islands have been a core national park, providing socal visitors a chance to visit nature in their own backyard. Unfortunately, from the pressures of various climate, the Channel Islands faces increased vulnerability to climate-induced 'shock' events, threatening diverse marine species and habitats. These events can include marine heat waves, and extreme increases (or decreases) in pH or dissolved oxygen. A collaborative project between UCSB and CINMS, funded by the National Oceanic and Atmospheric Association (NOAA), aims to develop climate-based indicators to enhance understanding and management. [link](www.youtube.com)
-         ![](ch_poster.jpg)
+        ## Purpose
+        The Channel Islands have been a core national park, providing people a chance to visit nature in their own backyard and are home to many diverse species and habitats. These habitats are highly sensitive to climate-driven ‘shock’ events which can include marine heat waves, and extreme fluctuations in pH or dissolved oxygen. Unfortunately, there few tools that currently exist that use climate data to create these visualizations easily. Current tools are satellite based and update from data that is gathered, such as the sanctuary watch tool provided by the [Channel Islands Sanctuary Watch website](https://sanctuarywatch.ioos.us/webcr-channelislands/). These do not give a future prediction of how these variables change, leading Channel Island staff to have limited information on creating a detailed action plan for possible restoration efforts.
+
+        A collaborative project between UCSB and [CINMS](https://channelislands.noaa.gov/), funded by the National Oceanic and Atmospheric Association [NOAA](https://www.noaa.gov/), aims to develop climate-based indicators to enhance understanding and management. This application creates three visualizations that can be found in the visualizations tab: a time series visualization, a vertical profile visualization, and a mapping visualization. Users can use the dropdown menu to pick their specifications to create a plot of their interest (ex. a time series of the sea surface temperature levels). These are meant to help aid the Channel Island staff to make educated and meaningful decisions to ensure the longevity of the island’s ecosystem.
+
+        It should be noted that this is a stepping stone towards creating an extensive visualization page to help see what certain climate conditions may look like in the future. It will allow others to carry on this project and use this prototype to cover more detailed information that may be of interest to Channel Island staff and other climate scientists that are interested in this project.
+
+
+
+
+
+
+        
+        This project was led by Dr. Samantha Stevenson-Michener, who leads the [Climate Data Lab](https://climate-datalab.org/) organization, which strives to break the barriers of those interested in getting into climate science research. This project was also led by Olivia Holt, Diana Navarro, and Patricia Park.
+
         """
+    ),
+        col_widths=(3, 8)
     ),
     ),
     ui.nav_panel(
         "Data", 
-        "A comprehensive Earth system model that simulates various components of the Earth's climate system, including the atmosphere, ocean, land surface, and sea ice."
+        ui.layout_columns(
+        ui.output_image("climate_image", inline=True),
+        ui.markdown(
+        """
+        ## Climate Model
+        A comprehensive Earth system model that simulates various components of the Earth's climate system, including the atmosphere, ocean, land surface, and sea ice.
+
+        ## Large Ensemble
+        A large ensemble is a set of simulations run with a given climate model which only vary the initial conditions. In this project the large ensemble used is CESM1 which is able to give historical, present and future readings of the outcome variables.
+
+        ## CESM-LE
+        Distinguishing between model error and natural climate variations can be tricky. To address this challenge, the CESM community developed the CESM Large Ensemble (CESM-LE) specifically to help understand climate change while considering natural fluctuations. All simulations within CESM-LE utilize a single CMIP5 model, which is the CESM with the Community Atmosphere Model version 5. The plots created in this dashboard were created using CESM version 1 (CESM1).
+
+        """
+    ),
+        col_widths=(3, 8)
+        ),
     ),
     ui.nav_panel(
         "Visualizations",
@@ -69,7 +102,7 @@ app_ui = ui.page_navbar(
                 ui.input_select(
                 id = "climate_variable_time",
                 label = "Climate Variable",
-                choices=climate_variable_choices
+                choices=climate_variable_choices_TEMP
         ),
             ui.input_select(
                 id = "experiment_choice_time",
@@ -87,7 +120,7 @@ app_ui = ui.page_navbar(
                 ui.input_select(
                 id = "climate_variable_vertical",
                 label = "Climate Variable",
-                choices=climate_variable_choices
+                choices=climate_variable_choices_TEMP
         ),
                 ui.input_select(
                 id = "experiment_choice_vertical",
@@ -105,7 +138,7 @@ app_ui = ui.page_navbar(
                 ui.input_select(
                 id = "climate_variable_map",
                 label = "Climate Variable",
-                choices=climate_variable_choices
+                choices=climate_variable_choices_SST
         ),
                 ui.input_select(
                 id = "experiment_choice_map",
@@ -180,11 +213,18 @@ def server(input, output, session):
         #returns plot
         return plot
 
-#render image to appear in web dashboard
+#render channel Islands poster to appear in web dashboard
     @render.image
     def ch_image():
-        img = {"src": here / "ch_poster.jpg", "width": "100px"}
+        img = {"src": here / "ch_poster.jpg", "width": "300px"}
         return img
+    
+
+#render climate model picture to appear in web dashboard
+    @render.image
+    def climate_image():
+        img_2 = {"src": here / "climate_model.jpeg", "width": "300px"}
+        return img_2
 
 
 
